424f5732fd00ec383b7e19406aaa61633c7af40c
lixuan <2789968443@qq.com>
2024-08-16 17:34:42 +0800

    feat(JavaStudy):CloudStudy-网关gateway

---
+++ SpringCloud/CloudStudy/hmall/hmall-gateway/src/main/java/com/hmall/gateway/HmallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务
+ * @author lixuan
+ */
+@SpringBootApplication
+public class HmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmallGatewayApplication.class, args);
+    }
+
+}
